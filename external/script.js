649439ab87e1063df903bf353adbb14586e36a96
Flavio Coutinho <fegemo@gmail.com>
2017-04-03 19:23:47 -0300

    Adds requried style.css header and makes functions.php load external files

--- external/script.js
+++ external/script.js
@@ -1,1 +1,33 @@
-console.log('loggin from external js');
+var inicializaFiltro = function() {
+  var filtroEl = document.querySelector(‘#filtro’);
+  if (!filtroEl) {
+    return;
+  }
+  var publicacoesEl = document.querySelectorAll(‘div.publicacao’);
+
+  function filtrar(evt) {
+    var textoDigitado = evt.currentTarget.value;
+
+    for (var i = 0; i < publicacoesEl.length; i++) {
+      var publicacaoEl = publicacoesEl[i];
+      publicacaoEl.classList.add('oculto');
+      var textoPublicacao = publicacaoEl.textContent;
+
+      if (textoPublicacao.toLowerCase().indexOf(textoDigitado.toLowerCase()) !== -1) {
+        publicacaoEl.classList.remove('oculto');
+      }
+    }
+    var publicacoesOcultasEl = document.querySelectorAll('div.publicacao.oculto');
+    var numeroDeResultados = publicacoesEl.length – publicacoesOcultasEl.length;
+    var resultadoEl = document.getElementById('resultados');
+    resultadoEl.innerHTML = numeroDeResultados + ' resultado(s)' ;
+    if(textoDigitado.length==0){
+      resultadoEl.classList.add('oculto');
+    }
+  }
+
+  filtroEl.addEventListener('change', filtrar);
+  filtroEl.addEventListener('input', filtrar);
+};
+
+document.addEventListener('DOMContentLoaded', inicializaFiltro);
